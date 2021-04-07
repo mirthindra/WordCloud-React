2ed881200af7d2a859977bcce441cb50b28c286c
mirthindra <mirthindra@gmail.com>
2021-04-07 21:15:27 +0900

    Initialize React Project

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+// import React from "react";
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h3>Hello World</h3>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello World</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
