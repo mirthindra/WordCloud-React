580a0acc3df3f7cbb8238d9491ea74cf2a866b4f
mirthindra <mirthindra@gmail.com>
2021-04-08 00:33:16 +0900

    Add Navigation Bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,9 @@
-// import React from "react";
-
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <h3>Hello World</h3>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
 import React, { Component } from "react";
+import AppShell from "./AppShell";
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <h3>Hello World</h3>
-      </div>
-    );
+    return <AppShell></AppShell>;
   }
 }
 
